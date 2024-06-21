05bf12b262fe228cb3d15a42beb44452c469cfdd
Wengtzuhsiu <163487746+Wengtzuhsiu@users.noreply.github.com>
2024-06-21 09:38:00 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,24 @@
+gsap.registerPlugin(ScrollTrigger) 
+// 告訴js有導入這個外掛
+
+gsap.to(".dot", {
+    scrollTrigger: '.dot',
+    // 捲到這個開始動
+    x: 10, 
+    rotation: 0,
+    y: 4,
+    duration: 3.5,
+    'background-color': 'gray',
+    'border-radius': '2px',
+    })
+    gsap.to(".sketching4", {
+        scrollTrigger: 'h1',
+        // 捲到這個開始動
+        x: 0, 
+        rotation: 0,
+        y: 225,
+        duration: 3.5,
+        'box-shadow': '5px 5px 5px #a9a9a900',
+    
+        })
+    
